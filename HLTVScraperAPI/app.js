9bb322aa76306e4494d4416597ae224be5da3b84
Renier Coetsee <renier.coetsee.rc@gmail.com>
2021-07-16 21:12:08 +0200

    Scrape and caching match pages

--- HLTVScraperAPI/app.js
+++ HLTVScraperAPI/app.js
@@ -26,8 +26,28 @@ async function getMatches() {
 }
 
 
-getMatches().then(function (matchUrls) {
-    console.log(matchUrls);
+async function cacheMatch(matchUrl) {
+    const url = baseUrl + matchUrl;
+    await rp(url).then(function (html) {
+        const fileName = './' + (matchUrl.substring(1) + '.html').replace(/\//g, '-');
+        console.log(fileName);
+        fs.writeFile(fileName, html, function(err) {
+            if(err) {
+                console.log(err);
+            }
+        });
+        console.log('file written');
+    }).catch(function (err) {
+            console.log(err);
+        });
+}
+
+const matchUrl = getMatches().then(function (matchUrls) {
+    console.log(matchUrls[0]);
+    for (let i = 0; i < matchUrls.length; i++) {
+        cacheMatch(matchUrls[i]);
+    }
 });
+
 
 
